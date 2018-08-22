4a93dc3b6715ab576c96c50423da3694b7304995
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-22 09:30:08 -0700

    added stack sorting, readme description

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cs-fundamentals
+
+Continuous work in progress, adding data structures and algorithms in JavaScript and Python as I study and write them.
--- data-structures/JavaScript/stack.js
+++ data-structures/JavaScript/stack.js
@@ -30,11 +30,36 @@ class Stack {
 }
 
 let testStack = new Stack();
-testStack.push('item one');
-testStack.push('item two');
+testStack.push(2);
+testStack.push(7);
+testStack.push(1);
+testStack.push(5)
 console.log(testStack.printStack());
 console.log(testStack.isEmpty());
 console.log(testStack.size());
 console.log(testStack.peek());
 console.log(testStack.pop());
+
+// Sorting a stack 
+const sortedInsert = (stack, item) => {
+  if(stack.length === 0 || item > stack[stack.length - 1]) {
+    stack.push(item);
+  } else {
+    let temp = stack.pop();
+    sortedInsert(stack, item);
+    stack.push(temp);
+  }
+}
+
+const sortStack = stack => {
+  if(stack.length > 0) {
+    let temp = stack.pop();
+    sortStack(stack);
+    sortedInsert(stack, temp);
+  }
+}
+
+let sampleStack = [14, 7, 1, 5];
+sortStack(sampleStack);
+console.log(sampleStack);
 
