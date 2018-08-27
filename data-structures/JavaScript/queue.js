965195b64ef7c4beac8eb10624140ca248019ff4
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-27 10:29:18 -0700

    added a stack implemented with two queues

--- data-structures/JavaScript/queue.js
+++ data-structures/JavaScript/queue.js
@@ -39,3 +39,45 @@ test.dequeue();
 console.log(test.store);
 console.log(test.size());
 console.log(test.top());
+
+// Implement a stack using only a queue
+class StackUsingQueue {
+  // use two queues to implement the stack
+  constructor() {
+    this.q1 = new Queue;
+    this.q2 = new Queue;
+  }
+
+  // add element to top of stack, same as normal stack operation
+  push(data) {
+    this.q1.enqueue(data);
+    return this.q1;
+  }
+  
+  // remove element from top of stack
+  pop() {
+    // move all q1 items (except the last) to q2
+    while(this.q1.size() > 1) {
+      this.q2.enqueue(this.q1.dequeue());
+    }
+    // remove last item and save it
+    let temp = this.q1.dequeue()
+    // move items back to q1
+    let q = new Queue;
+    this.q1 = this.q2;
+    this.q2 = q;
+
+    // return the popped item
+    return temp;
+  }
+
+}
+
+let newTest = new StackUsingQueue;
+newTest.push(1);
+newTest.push(2);
+newTest.push(3);
+console.log(newTest.pop());
+console.log(newTest.q1);
+newTest.push(4);
+console.log(newTest.q1);
